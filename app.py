e5199ea03733cd0205c26ff11771d51605518374
Chris Egersdoerfer <cegersdoerfer@gmail.com>
2024-02-14 19:48:08 -0500

    add chat api

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ import streamlit as st
 import streamlit.components.v1 as components
 from io import StringIO
 from parse_trace import parse_to_df, create_prompt
+from chatUtils import open_client, generate_analysis, ISSUE_LABELS
+import os
+
 
 # Sidebar
 st.sidebar.title("Options")
 openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
+os.environ["OPENAI_API_KEY"] = openai_api_key
+
+chat_client = open_client()
 
 st.sidebar.header("Issues to Analyze: ")
 
@@ -14,8 +20,15 @@ random_io = st.sidebar.checkbox("Random I/O")
 load_imbalanced_io = st.sidebar.checkbox("Load Imbalance")
 shared_file_io = st.sidebar.checkbox("Shared File I/O")
 
+issues = {
+    ISSUE_LABELS["small_io"]: small_io,
+    ISSUE_LABELS["random_io"]: random_io,
+    ISSUE_LABELS["load_imbalanced_io"]: load_imbalanced_io,
+    ISSUE_LABELS["shared_file_io"]: shared_file_io
+}
 
-def parse_file(uploaded_file, issues):
+
+def parse_file(uploaded_file):
     if uploaded_file is None:
         st.warning("Please make sure you uploaded a proper Darshan trace!", icon="⚠")
     else:
@@ -26,17 +39,11 @@ def parse_file(uploaded_file, issues):
                 stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
                 string_data = stringio.read()
                 df, trace_start_time, full_runtime = parse_to_df(string_data)
-                df.to_csv(f'csv/{uploaded_file.name.split(".")[0]}.csv', index=False)
-
-                for issue, value in issues.items():
-                    if value:
-                        prompt = create_prompt(string_data, df, issue)
-                        prompt_name = f"{issue} - {uploaded_file.name.split('.')[0]}"
-
-                        with open(f"prompts/{prompt_name}", 'w') as f:
-                            f.write(prompt)
+                file_path = f'csv/{uploaded_file.name.split(".")[0]}.csv'
+                df.to_csv(file_path, index=False)
 
                 st.success("File successfully parsed and saved!", icon="✅")
+                return file_path
             except Exception as e:
                 st.exception(f"I am sorry. Something wrong occurred, please try again: {e}")
 
@@ -52,6 +59,7 @@ components.html(
     """
 )
 
+new_file = None
 with st.form("main_form"):
     uploaded_file = st.file_uploader("Please enter your Darshan DXT Trace (txt files only)")
     submit = st.form_submit_button("Analyze Darshan trace!")
@@ -59,11 +67,20 @@ with st.form("main_form"):
     if not openai_api_key.startswith("sk-"):
         st.warning("Please make sure a proper OpenAI API Key is entered!", icon="⚠")
 
-    if openai_api_key.startswith("sk-") and submit:
-        issues = {
-            "small_io": small_io,
-            "random_io": random_io,
-            "load_imbalanced_io": load_imbalanced_io,
-            "shared_file_io_extended": shared_file_io
-        }
-        parse_file(uploaded_file, issues)
+    if openai_api_key.startswith("sk-") and uploaded_file is not None:
+        new_file = parse_file(uploaded_file)
+        print(new_file)
+
+    if submit and new_file is not None:
+        selected_issues = [issue for issue, value in issues.items() if value]
+        print(selected_issues)
+        diagnoses, summary, failures = generate_analysis(chat_client, new_file, selected_issues)
+        print(summary)
+        for i, issue in enumerate(diagnoses.keys()):
+            with st.expander(f"{selected_issues[i]} Diagnosis: "):
+                st.markdown(diagnoses[issue])
+        if summary:
+            st.markdown(f"## Summary: \n{summary}")
+
+
+
